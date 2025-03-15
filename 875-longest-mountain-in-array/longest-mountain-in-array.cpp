4c42d56e7613201a0bddc7e7d96fc05dd58fe8ab
uday899 <udayavula1234@gmail.com>
2025-03-15 18:54:42 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (79.92%) - LeetSync

---
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        int n=arr.size();
+        int largest=0;
+        if(n<3) return 0;
+        for(int i=1;i<n-1;i++){
+           if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+             int left=i-1;
+             int right=i+1;
+             while(left>0 && arr[left]>arr[left-1]){
+                left--;
+             }
+             while(right<n-1 && arr[right]>arr[right+1]){
+                right++;
+
+             }
+             int length=right-left+1;
+             largest =max(largest,length);
+
+           } 
+        }
+         return largest;
+    }
+};
