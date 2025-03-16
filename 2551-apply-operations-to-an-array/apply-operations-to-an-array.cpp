c2ae2e5a9b28c15f9b86838b072ec03f52567ca8
uday899 <udayavula1234@gmail.com>
2025-03-16 19:51:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (88.02%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         int j=0;
         for(int i=0;i<n;i++){
-            if( i+1< n && nums[i]==nums[i+1] && nums[i]!=0){
+            if( i+1< n && nums[i]==nums[i+1]){
                  nums[i]=nums[i]*2;
                  nums[i+1]=0;
             }
