4713b2541c116ee910dc1f779788b201f9341efb
uday899 <udayavula1234@gmail.com>
2025-03-22 16:11:48 +0530

    Time: 3 ms (17.03%) | Memory: 11.8 MB (55.82%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        vector<int>ans;
+        int l=0;
+        int h=s.size();
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='I'){
+                ans.push_back(l++);
+            }
+            else{
+                ans.push_back(h--);
+            }
+         
+        }
+        ans.push_back(h);
+        return ans;
+    }
+};
