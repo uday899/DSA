bd4a8c230a60488178e917b8b8ebf7b27e220193
uday899 <udayavula1234@gmail.com>
2025-04-05 23:14:37 +0530

    Time: 70 ms (77.89%) | Memory: 87.4 MB (80.69%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -3,15 +3,18 @@ public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
         int n=nums.size();
         vector<int>result(n);
-        vector<int>prefixsum(n);
-        prefixsum[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefixsum[i]=prefixsum[i-1]+nums[i];
-        }
+        /* vector<int>prefixsum(n);
+        prefixsum[0]=nums[0];              0(n)-->extra space now i will
+        for(int i=1;i<n;i++){           optimize this with out extra
+            prefixsum[i]=prefixsum[i-1]+nums[i];  array by cal prefix within arr
+        } */                                       
+          int sum=accumulate(begin(nums),end(nums),0);
+          int prefix=0;
         for(int i=0;i<n;i++){
-            int leftsum=prefixsum[i]-nums[i];
-            int rightsum=prefixsum[n-1]-prefixsum[i];
+            int leftsum=prefix;              //prefixsum[i]-nums[i];
+            int rightsum =sum-prefix-nums[i];                       //prefixsum[n-1]-prefixsum[i];
             result[i]=(nums[i]*i)-leftsum + rightsum-(nums[i]*(n-i-1));
+            prefix+=nums[i];
         }
         return result;
     }
