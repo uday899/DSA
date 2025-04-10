ed622fda267d804b12648a7d0f9aba66f1fd5444
uday899 <udayavula1234@gmail.com>
2025-04-10 21:54:10 +0530

    Time: 4 ms (4.55%) | Memory: 11.9 MB (55.33%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int>mpp;
-        for(auto i: arr){
-            mpp[i]++;
-        }
-       unordered_set<int>freqset;
-       for(auto value:mpp){
-        freqset.insert(value.second);
-       } 
+      unordered_map<int,int>freq;
+      for(int num:arr){
+        freq[num]++;
+      }
+    unordered_set<int>freqset;
+    for(auto it:freq){
+        freqset.insert(it.second);
 
-        return freqset.size()==mpp.size();
+    }
+    return freq.size()==freqset.size();
     }
 };
